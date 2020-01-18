48b37cd53c61f47de885c55fbd1483aaaac5727d
Mark Adrian T. Layones <Mtlayones@up.edu.ph>
2020-01-18 15:21:11 +0800

    Add files via upload

---
+++ KimPossibleandTheMooks2(revised).py
@@ -0,0 +1,30 @@
+# def consecutiveChecker(a,b):
+
+userInput = list(map(int,input().split()))
+iterations = userInput[0]
+swaps = userInput[1]
+binaryNumberList = []
+ones = []
+zeros = []
+
+for k in range(0,iterations):
+	userInput = input()
+	if(userInput == "MOOK"):
+		binaryNumberList.insert(0,"1")
+		ones.insert(0,k)
+	else:
+		binaryNumberList.insert(0,"0")
+		zeros.append(k)
+
+print(ones)
+print(zeros)
+
+for k in range(0,swaps):
+	if(zeros[k] < ones[k]):
+		binaryNumberList[(iterations - 1) - ones[k]] = "0"
+		binaryNumberList[(iterations - 1) - zeros[k]] = "1"
+	else:
+		break
+
+binaryNumber = "".join(binaryNumberList)
+print(int(binaryNumber,2))

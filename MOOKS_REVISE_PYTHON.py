b0c29b40fda6cbcf63e3f817babf43f8483fa009
ridiculoys <43093255+ridiculoys@users.noreply.github.com>
2020-01-20 16:13:07 +0800

    Add files via upload

---
+++ MOOKS_REVISE_PYTHON.py
@@ -0,0 +1,28 @@
+userInput = list(map(int,input().split()))
+iterations = userInput[0]
+swaps = userInput[1]
+binaryNumberList = []
+ones = []
+zeros = []
+ 
+for k in range(0,iterations):
+	userInput = input()
+	if(userInput == "MOOK"):
+		binaryNumberList.insert(0,"1")
+		ones.insert(0,k)
+	else:
+		binaryNumberList.insert(0,"0")
+		zeros.append(k)
+ 
+for k in range(0,swaps):
+	try:
+		if(zeros[k] < ones[k]):
+			binaryNumberList[(iterations - 1) - ones[k]] = "0"
+			binaryNumberList[(iterations - 1) - zeros[k]] = "1"
+		else:
+			break
+	except:
+		break
+ 
+binaryNumber = "".join(binaryNumberList)
+print(int(binaryNumber,2)%1000000000)
